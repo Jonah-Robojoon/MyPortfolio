8e632cbd548803b19469dc58c105e2cc87be3a88
Jonah-Robojoon <38752@ma-web.nl>
2026-04-14 21:37:49 +0200

    only need CV

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,26 +1,16 @@
-﻿        let slideIndex = 1;
-        showSlides(slideIndex);
+﻿const gameUrls = [
+    "Html/StoreOfSteam.html",
+    "Html/PlantsVRBaloons.html",
+    "Html/UltrakillRemake.html",
+    "Html/Gravity.html",
+    "Html/GlacialRailways.html",
+    "Html/GlacialRailways.html",
+    "Html/GlacialRailways.html",
+    "Html/GlacialRailways.html",
+    "Html/GlacialRailways.html"
+];
 
-        function plusSlides(n) {
-        showSlides(slideIndex += n);
-        }
-
-        function currentSlide(n) {
-        showSlides(slideIndex = n);
-        }
-
-        function showSlides(n) {
-        let i;
-        let slides = document.getElementsByClassName("mySlides");
-        let dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex-1].style.display = "flex";
-        dots[slideIndex-1].className += " active";
-        }
+function goToRandomGame() {
+    const randomIndex = Math.floor(Math.random() * gameUrls.length);
+    window.location.href = gameUrls[randomIndex];
+}
